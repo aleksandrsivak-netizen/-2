f0a2195c2c6471081e39e45189f9fc9eed3e3f25
Хеж Алим Бисланович <alimhezhh@gmail.com>
2026-06-28 03:25:05 +0300

    Update tercom_uav/src/tercom_uav/__init__.py

--- tercom_uav/src/tercom_uav/__init__.py
+++ tercom_uav/src/tercom_uav/__init__.py
@@ -1,7 +1,19 @@
-"""Terrain-referenced navigation prototype for UAV radio-altimeter streams."""
+﻿"""Terrain-referenced navigation prototype for UAV radio-altimeter streams."""
 
-from tercom_uav.config import CorrelationConfig, KalmanConfig, SimulationConfig
+from tercom_uav.config import CorrelationConfig, GPSFusionConfig, KalmanConfig, SimulationConfig
+from tercom_uav.dorabotka import DorabotkaSearchConfig, run_dorabotka
 from tercom_uav.dem import DEMGrid
+from tercom_uav.gps import GPSFix, GPSFusionState, NavigationMode
 
-__all__ = ["CorrelationConfig", "DEMGrid", "KalmanConfig", "SimulationConfig"]
-
+__all__ = [
+    "DorabotkaSearchConfig",
+    "CorrelationConfig",
+    "DEMGrid",
+    "GPSFix",
+    "GPSFusionConfig",
+    "GPSFusionState",
+    "KalmanConfig",
+    "NavigationMode",
+    "SimulationConfig",
+    "run_dorabotka",
+]
